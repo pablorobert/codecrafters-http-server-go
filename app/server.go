0c53d0e8542110efd062b3234e7b95691e9fb40a
Pablo Alves <pablorobert@gmail.com>
2024-05-15 22:44:24 -0300

    /echo

--- app/server.go
+++ app/server.go
@@ -37,16 +37,28 @@ func main() {
 	str := string(buf)
 	lines := strings.Split(str, "\r\n")
 	header := lines[0]
-	fmt.Println(header)
-	parts := strings.Split(header, " ")
-	if (parts[0] == "GET") {
-		request := parts[1]
-		fmt.Println("GET Request", request)
-		if request[0:1] == "/" && len(request) > 1 {
-			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-		} else if (request == "/") {
-			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-		}
+	if (header[0:3] == "GET") {
+		ParseGetRequest(header, conn)
 	}
+}
+
+func ParseGetRequest(header string, conn net.Conn) {
+	parts := strings.Split(header, " ")
+	request := parts[1]
 	
+	if request[0:1] == "/" && len(request) > 1 {
+		if len(request) > 6 && request[0:6] == "/echo/" {
+			EchoEndPoint(request[6:], conn)
+		} else {
+			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		}
+	} else if (request == "/") {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	}
+}
+
+func EchoEndPoint(echo string, conn net.Conn) {
+	str := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(echo),echo)
+	fmt.Println(str)
+	conn.Write([]byte(str))
 }
