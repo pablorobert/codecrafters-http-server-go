d5bbfdc49fe3e636f8aed5cd511b797504bf702a
Pablo Alves <pablorobert@gmail.com>
2024-05-16 17:28:01 -0300

    Concurrency

--- app/server.go
+++ app/server.go
@@ -9,23 +9,28 @@ import (
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-	
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
 	
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
 
+		go Server(conn)
+	}
+	
+}
+
+func Server(conn net.Conn) {
+	defer conn.Close()
 	buf := make([]byte, 1024)
-	_, err = conn.Read(buf)
+	_, err := conn.Read(buf)
 	if err != nil {
 		if err != io.EOF {
 			fmt.Println("read error:", err)
@@ -33,12 +38,10 @@ func main() {
 			os.Exit(1)
 		}
 	}
-
 	str := string(buf)
 	lines := strings.Split(str, "\r\n")
 	header := lines[0]
 	headers := lines[1:]
-	fmt.Println(headers)
 	if (header[0:3] == "GET") {
 		ParseGetRequest(header, headers, conn)
 	}
@@ -55,7 +58,6 @@ func ParseGetRequest(header string, headers []string, conn net.Conn) {
 
 	if request[0:1] == "/" && len(request) > 1 {
 		if len(request) >= 11 && request[0:11] == "/user-agent" {
-			fmt.Println("UserAgent")
 			UserAgentEndPoint(request[11:], headers, conn)
 		} else if len(request) >= 6 && request[0:6] == "/echo/" {
 			EchoEndPoint(request[6:], conn)
@@ -71,18 +73,13 @@ func UserAgentEndPoint(echo string, headers []string, conn net.Conn) {
 			parts := strings.Split(header, ":")
 			userAgent := strings.TrimSpace(parts[1])
 			str := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(userAgent), userAgent)
-			fmt.Println(str)
 			conn.Write([]byte(str))
 			return
 		}
 	}
-	/*str := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(echo),echo)
-	fmt.Println(str)
-	conn.Write([]byte(str))*/
 }
 
 func EchoEndPoint(echo string, conn net.Conn) {
 	str := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(echo),echo)
-	fmt.Println(str)
 	conn.Write([]byte(str))
 }
