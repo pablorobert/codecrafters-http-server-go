f53da671881e978c655f09bb70e902d3bc4870da
Pablo Alves <pablorobert@gmail.com>
2024-05-16 17:46:39 -0300

    GET /files

--- app/server.go
+++ app/server.go
@@ -8,13 +8,30 @@ import (
 	"strings"
 )
 
+type config struct {
+	directory string
+}
+
+var settings config
+
+func init() {
+	settings = config{}
+}
+
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	
+
+	argsWithoutProgram := os.Args[1:]
+	for i := 0; i < len(argsWithoutProgram); i++ {
+		if (argsWithoutProgram[i] == "--directory") {
+			settings.directory = argsWithoutProgram[i+1]
+		}
+	}
+
 	for {
 		conn, err := l.Accept()
 		if err != nil {
@@ -59,6 +76,8 @@ func ParseGetRequest(header string, headers []string, conn net.Conn) {
 	if request[0:1] == "/" && len(request) > 1 {
 		if len(request) >= 11 && request[0:11] == "/user-agent" {
 			UserAgentEndPoint(request[11:], headers, conn)
+		} else if len(request) >= 7 && request[0:7] == "/files/" {
+			FileEndPoint(request[7:], conn)
 		} else if len(request) >= 6 && request[0:6] == "/echo/" {
 			EchoEndPoint(request[6:], conn)
 		} else {
@@ -77,6 +96,23 @@ func UserAgentEndPoint(echo string, headers []string, conn net.Conn) {
 			return
 		}
 	}
+}
+
+func FileEndPoint(filePath string, conn net.Conn) {
+	fullPath := settings.directory + filePath
+	if fi, err := os.Stat(fullPath); err == nil {
+		content, err := os.ReadFile(fullPath)
+		if err != nil {
+			os.Exit(-3)
+			//"Erro ao ler conteudo do arquivo"
+		}
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n"))
+		conn.Write([]byte("Content-Length: " + fmt.Sprintf("%d", fi.Size()) + "\r\n"))
+		conn.Write([]byte("Content-Type: application/octet-stream\r\n\r\n"))
+		conn.Write(content)
+		return
+	}
+	conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 }
 
 func EchoEndPoint(echo string, conn net.Conn) {
