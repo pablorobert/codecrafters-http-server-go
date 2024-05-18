3737a94c38be3b2bd67f288081e6d504ea8bd2a6
Pablo Alves <pablorobert@gmail.com>
2024-05-17 21:49:37 -0300

    Refactoring

--- app/server.go
+++ app/server.go
@@ -16,12 +16,75 @@ type config struct {
 	directory string
 }
 
+type httpRequest struct {
+	httpMethod string
+	httpPath string
+	paths []string
+	httpVersion string
+	headers map[string]string
+	conn net.Conn
+}
+
+type httpResponse struct {
+	httpStatusCode int
+	httpStatusDescption string
+	headers []string
+	body string
+}
+
+type statusCodeMap struct {
+	status int
+	description string
+}
+
+var statusMap = map[int]string{
+	200: "OK",
+	201: "Created",
+	400: "Bad Request",
+	404: "Not Found",
+	500: "Internal Server Error",
+}
+
+func NewHTTPResponse(status int) *httpResponse {
+	description := statusMap[status]
+	return &httpResponse{
+		httpStatusCode: status,
+		httpStatusDescption: description,
+		headers: []string{},
+	}
+}
+
+func (r *httpResponse) addHeader(header string) {
+	r.headers = append(r.headers, header)
+}
+
+func (r *httpResponse) sendResponse(conn net.Conn) {
+	firstLine := fmt.Sprintf("HTTP/1.1 %d %s\r\n", 
+		r.httpStatusCode, 
+		r.httpStatusDescption)
+	conn.Write([]byte(firstLine))
+
+	for _, v := range r.headers {
+		conn.Write([]byte(fmt.Sprintf("%s\r\n", v)))
+	}
+
+	if (len(r.body) > 0) {
+		conn.Write([]byte("\r\n"))
+		conn.Write([]byte(r.body))
+	}
+
+	conn.Write([]byte("\r\n"))
+}	
+
 var httpHeaders map[string]string = make(map[string]string)
 
 var settings config
-
 func init() {
 	settings = config{}
+}
+
+func NewHTTPRequest() httpRequest {
+	return httpRequest{}
 }
 
 func main() {
@@ -31,10 +94,9 @@ func main() {
 		os.Exit(1)
 	}
 
-	argsWithoutProgram := os.Args[1:]
-	for i := 0; i < len(argsWithoutProgram); i++ {
-		if (argsWithoutProgram[i] == "--directory") {
-			settings.directory = argsWithoutProgram[i+1]
+	for i := 1; i < len(os.Args); i++ {
+		if (os.Args[i] == "--directory") {
+			settings.directory = os.Args[i + 1]
 		}
 	}
 
@@ -44,7 +106,6 @@ func main() {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
-
 		go Server(conn)
 	}
 	
@@ -52,6 +113,7 @@ func main() {
 
 func Server(conn net.Conn) {
 	defer conn.Close()
+
 	buf := make([]byte, 1024)
 	_, err := conn.Read(buf)
 	if err != nil {
@@ -61,81 +123,81 @@ func Server(conn net.Conn) {
 			os.Exit(1)
 		}
 	}
-	str := string(buf)
-	lines := strings.Split(str, "\r\n")
-	requestHeader := lines[0]
-	headers := lines[1:]
-	ReadHTTPHeaders(headers)
 
-	if (requestHeader[0:3] == "GET") {
-		ParseGetRequest(requestHeader, headers, conn)
+	request := NewHTTPRequest()
+	request.conn = conn
+	lines := strings.Split(string(buf), "\r\n")
+
+	ReadHTTPHeaders(&request, lines[1:])
+	fmt.Sscanf(lines[0], "%s %s %s", &request.httpMethod, &request.httpPath, &request.httpVersion)
+	
+	if (request.httpPath == "/") {
+		request.paths = []string{"/"}
+	} else {
+		path := request.httpPath[1:]
+		request.paths = strings.Split(path, "/")
 	}
-	if (requestHeader[0:4] == "POST") {
-		ParsePostRequest(requestHeader, headers, conn)
+
+	if (request.httpMethod == "GET") {
+		ParseGetRequest(request)
+	}
+
+	if (request.httpMethod == "POST") {
+		ParsePostRequest(request, lines[1:])
 	}
 }
 
-func ReadHTTPHeaders(headers []string) {
+func ReadHTTPHeaders(request *httpRequest, headers []string) {
 	for i := 0; i < len(headers); i++ {
 		if headers[i] == "" {
 			break
 		}
-		breakHeader(headers[i])
+		parts := strings.Split(headers[i], ":")
+		key := strings.ToLower(strings.TrimSpace(parts[0]))
+		value := strings.TrimSpace(parts[1])
+		httpHeaders[key] = strings.Replace(value, "\r\n", "", -1)
 	}
+	request.headers = httpHeaders
 }
 
-func breakHeader(header string) {
-	parts := strings.Split(header, ":")
-	key := strings.ToLower(strings.TrimSpace(parts[0]))
-	value := strings.TrimSpace(parts[1])
-	httpHeaders[key] = value
-}
-
-func ParseGetRequest(header string, headers []string, conn net.Conn) {
-	parts := strings.Split(header, " ")
-	request := parts[1]
-	
-	if request == "/" && len(request) == 1 {
-		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+func ParseGetRequest(request httpRequest) {
+	if request.paths[0] == "/" {
+		NewHTTPResponse(200).sendResponse(request.conn)
 		return
-	}
-
-	if request[0:1] == "/" && len(request) > 1 {
-		if len(request) >= 11 && request[0:11] == "/user-agent" {
-			UserAgentEndPoint(request[11:], headers, conn)
-		} else if len(request) >= 7 && request[0:7] == "/files/" {
-			FileEndPoint(request[7:], conn)
-		} else if len(request) >= 6 && request[0:6] == "/echo/" {
-			EchoEndPoint(request[6:], conn)
-		} else {
-			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	} else {
+		endpoint := request.paths[0]
+		if endpoint == "user-agent" {
+			UserAgentEndPoint(request)
+			return
 		}
+		if endpoint == "files" {
+			FileEndPoint(request)
+			return
+		}
+		if endpoint == "echo" {
+			EchoEndPoint(request)
+			return
+		}
+		NewHTTPResponse(404).sendResponse(request.conn)
 	}
 }
 
-func ParsePostRequest(header string, headers []string, conn net.Conn) {
-	parts := strings.Split(header, " ")
-	request := parts[1]
+func ParsePostRequest(request httpRequest, headers []string) {
 	
 	var fileContent string =""
 	var buffer bytes.Buffer
 	var length int
 	var err error
 
-	for _, header := range headers {
-		if strings.Contains(header, "Content-Length") {
-			parts = strings.Split(header, ": ")
-			length, err = strconv.Atoi(parts[1])
-			if err != nil {
-				length = 0
-			}
+	if contentLength, ok := request.headers["content-length"]; ok {
+		length, err = strconv.Atoi(contentLength)
+		if err != nil {
+			length = 0
 		}
 	}
 
 	idx := 0
-	fmt.Println("len", len(headers))
 	for i := 0; i < len(headers); i++ {
-		fmt.Println(headers[i])
 		if (headers[i] == "") {
 			idx = i
 		}
@@ -148,61 +210,58 @@ func ParsePostRequest(header string, headers []string, conn net.Conn) {
 	}
 	fileContent = buffer.String()
 
-	if request[0:1] == "/" && len(request) > 1 {
-		if len(request) >= 7 && request[0:7] == "/files/" {
-			PostFileEndPoint(request[7:], fileContent, conn)
-		} else {
-			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-		}
+	endpoint := request.paths[0]
+		
+	if endpoint == "files" {
+		PostFileEndPoint(request, fileContent)
+		return
+	}
+	NewHTTPResponse(404).sendResponse(request.conn)
+}
+
+func UserAgentEndPoint(request httpRequest) {
+	response := NewHTTPResponse(200)
+	if userAgent, ok := request.headers["user-agent"]; ok {
+		response.addHeader("Content-Type: text/plain")
+		response.addHeader(fmt.Sprintf("Content-Length: %d", len(userAgent)))
+		response.body = userAgent
+
+		response.sendResponse(request.conn)
 	}
 }
 
-func UserAgentEndPoint(echo string, headers []string, conn net.Conn) {
-	for _, header := range headers {
-		if strings.Contains(header, "User-Agent") {
-			parts := strings.Split(header, ":")
-			userAgent := strings.TrimSpace(parts[1])
-			str := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(userAgent), userAgent)
-			conn.Write([]byte(str))
-			return
-		}
-	}
-}
-
-func PostFileEndPoint(filePath string, fileContent string, conn net.Conn) {
-	fullPath := settings.directory + filePath
-	fmt.Println(fullPath)
+func PostFileEndPoint(request httpRequest, fileContent string) {
+	fullPath := settings.directory + request.paths[1]
 	file, err := os.Create(fullPath)
-	/*err := os.WriteFile(fullPath, []byte(fileContent), 0644)*/
 	if err != nil {
-		os.Exit(-2)
-		//"Erro ao criar arquivo"
+		os.Exit(-2) //"Erro ao criar arquivo"
 	}
 	defer file.Close()
 	_, err = file.WriteString(fileContent)
 	if err != nil {
-		os.Exit(-2)
-		//"Erro ao escrever arquivo"
+		os.Exit(-2) //"Erro ao escrever arquivo"
 	}
-	conn.Write([]byte("HTTP/1.1 201 Created\r\n\r\n"))
-	//conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	NewHTTPResponse(201).sendResponse(request.conn)
 }
 
-func FileEndPoint(filePath string, conn net.Conn) {
+func FileEndPoint(request httpRequest) {
+	filePath := request.paths[1]
 	fullPath := settings.directory + filePath
 	if fi, err := os.Stat(fullPath); err == nil {
 		content, err := os.ReadFile(fullPath)
 		if err != nil {
-			os.Exit(-3)
-			//"Erro ao ler conteudo do arquivo"
+			os.Exit(-3) //"Erro ao ler conteudo do arquivo"
 		}
-		conn.Write([]byte("HTTP/1.1 200 OK\r\n"))
-		conn.Write([]byte("Content-Length: " + fmt.Sprintf("%d", fi.Size()) + "\r\n"))
-		conn.Write([]byte("Content-Type: application/octet-stream\r\n\r\n"))
-		conn.Write(content)
+
+		response := NewHTTPResponse(200)
+		response.addHeader("Content-Type: application/octet-stream")
+		response.addHeader("Content-Length: " + fmt.Sprintf("%d", fi.Size()))
+		response.body = string(content)
+		
+		response.sendResponse(request.conn)
 		return
 	}
-	conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	NewHTTPResponse(404).sendResponse(request.conn)
 }
 
 func GzipString(str string) string {
@@ -217,18 +276,22 @@ func GzipString(str string) string {
 	return b.String()
 }
 
-func EchoEndPoint(echo string, conn net.Conn) {
-	encoding := httpHeaders["accept-encoding"]
-	var str string
-	if (encoding == "gzip" || strings.Contains(encoding, "gzip")) {
-		encoding = "gzip"
-		gzipped := GzipString(echo)
+func EchoEndPoint(request httpRequest) {
+	response := NewHTTPResponse(200)
+	response.addHeader("Content-Type: text/plain")
+	echo := request.paths[1]
 
-		str = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Encoding: %s\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
-			encoding, len(gzipped), gzipped)
-	} else {
-		str = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
-			len(echo),echo)
+	if encoding, ok := request.headers["accept-encoding"]; ok {
+		if (encoding == "gzip" || strings.Contains(encoding, "gzip")) {
+			gzipped := GzipString(echo)
+	
+			response.addHeader("Content-Encoding: gzip")
+			response.addHeader("Content-Length: " + fmt.Sprintf("%d", len(gzipped)))
+			response.body = gzipped
+		} else {
+			response.addHeader("Content-Length: " + fmt.Sprintf("%d", len(echo)))
+			response.body = echo
+		}
 	}
-	conn.Write([]byte(str))
+	response.sendResponse(request.conn)
 }
